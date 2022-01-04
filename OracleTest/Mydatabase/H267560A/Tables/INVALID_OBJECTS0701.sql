0ae5e73008cd126c2ec9033b2102bda764b5115a
H478932 <H478932@>
2022-01-04 18:52:25 +0530

    Test

---
+++ OracleTest/Mydatabase/H267560A/Tables/INVALID_OBJECTS0701.sql
@@ -0,0 +1,17 @@
+CREATE TABLE h267560a.invalid_objects0701 (
+  "OWNER" VARCHAR2(30 BYTE),
+  object_name VARCHAR2(128 BYTE),
+  subobject_name VARCHAR2(30 BYTE),
+  object_id NUMBER,
+  data_object_id NUMBER,
+  object_type VARCHAR2(19 BYTE),
+  created DATE,
+  last_ddl_time DATE,
+  "TIMESTAMP" VARCHAR2(19 BYTE),
+  status VARCHAR2(7 BYTE),
+  "TEMPORARY" VARCHAR2(1 BYTE),
+  "GENERATED" VARCHAR2(1 BYTE),
+  secondary VARCHAR2(1 BYTE),
+  "NAMESPACE" NUMBER,
+  edition_name VARCHAR2(30 BYTE)
+);
---
+++ OracleTest/Mydatabase/H267560A/Tables/REFRESH_TEST.sql
@@ -0,0 +1,5 @@
+CREATE TABLE h267560a.refresh_test (
+  o_name VARCHAR2(20 BYTE),
+  t_name VARCHAR2(50 BYTE),
+  cnt NUMBER
+);
