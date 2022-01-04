73ec55ec490c6ed39395e72b9382ef2f0f156140
H478932 <H478932@>
2022-01-04 19:09:19 +0530

    add col 2

--- OracleTest/Mydatabase/H267560A/Tables/REFRESH_TEST.sql
+++ OracleTest/Mydatabase/H267560A/Tables/REFRESH_TEST.sql
@@ -2,5 +2,6 @@ CREATE TABLE h267560a.refresh_test (
   o_name VARCHAR2(20 BYTE),
   t_name VARCHAR2(50 BYTE),
   cnt NUMBER,
-  dent DATE DEFAULT sysdate
+  dent DATE DEFAULT sysdate,
+  modified_date DATE
 );
